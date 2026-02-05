3d31e722c3036805d201f2ec3ca3e7617e500ab3
Krishna4593 <kamalkrishna489@gmail.com>
2026-02-05 23:53:45 +0530

    updated api added

--- Day9-rev/Frontend/src/App.jsx
+++ Day9-rev/Frontend/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   const [notes, setNotes]=useState([])
 
 function fetchNotes(){
-  axios.get("http://localhost:3000/api/notes")
+  axios.get("https://backend-class-2-eiaq.onrender.com/api/notes")
 .then((res)=>{
   
   setNotes(res.data.notes)
@@ -21,7 +21,7 @@ useEffect(()=>{
   e.preventDefault();
   const {title, description}= e.target.elements
   console.log(title.value, description.value)
-  axios.post("http://localhost:3000/api/notes", {
+  axios.post("https://backend-class-2-eiaq.onrender.com/api/notes", {
     title: title.value,
     description: description.value
   })
@@ -32,7 +32,7 @@ fetchNotes()
 
   function deleteHandler(noteId){
 
-axios.delete("http://localhost:3000/api/notes/"+noteId)
+axios.delete("https://backend-class-2-eiaq.onrender.com/api/notes/"+noteId)
 .then(()=>{
   fetchNotes()
 })
